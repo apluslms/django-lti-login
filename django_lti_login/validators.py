a4435d67cd671108ea107bfdb02afb6f8a903867
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-09-12 18:03:57 +0300

    Fix validator to work with invalid client keys

--- django_lti_login/validators.py
+++ django_lti_login/validators.py
@@ -3,7 +3,13 @@ from oauthlib.oauth1 import RequestValidator
 from . import SAFE_CHARACTERS_SET, KEY_LENGTH
 from .models import LTIClient
 
+
 class LTIRequestValidator(RequestValidator):
+    """
+    LTIRequestValidator.
+
+    Supports only SignatureOnlyEndpoint
+    """
     def __init__(self):
         self.__secrets = {}
 
@@ -27,6 +33,17 @@ class LTIRequestValidator(RequestValidator):
         """ Allow unsafe access when using limited network. """
         return False
 
+    # used by: SignatureOnlyEndpoint
+    @property
+    def dummy_client(self):
+        """
+        Return dummy client key.
+        This can't be valid key in database.
+        All keys in database are at least 6 characters
+        """
+        return "DEAD"
+
+    # used by: SignatureOnlyEndpoint
     def validate_timestamp_and_nonce(self, client_key, timestamp, nonce,
         request_token=None, access_token=None):
         """
@@ -35,12 +52,24 @@ class LTIRequestValidator(RequestValidator):
         """
         return True
 
+    # used by: SignatureOnlyEndpoint
     def validate_client_key(self, client_key, request):
         return self.get_client_secret(client_key, request) is not None
 
+    # used by: SignatureOnlyEndpoint
     def get_client_secret(self, client_key, request):
         if client_key in self.__secrets:
             return self.__secrets[client_key]
-        secret = LTIClient.objects.filter(key=client_key).first().secret
+        try:
+            secret = LTIClient.objects.values('secret').get(key=client_key)['secret']
+        except LTIClient.DoesNotExist:
+            if client_key == "DEAD":
+                return "DEAD"
+            return None
         self.__secrets[client_key] = secret
         return secret
+
+    # used by: SignatureOnlyEndpoint
+    def get_rsa_key(self, client_key, request):
+        # No support for rsa keys
+        return None
