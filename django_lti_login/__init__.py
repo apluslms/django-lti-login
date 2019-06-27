a9d5c447638b48a007de72f928ae36e0a5941705
Jaakko Kantojärvi <jaakko@n-1.fi>
2019-06-28 00:59:26 +0300

    Reduce key limits a bit

--- django_lti_login/__init__.py
+++ django_lti_login/__init__.py
@@ -2,8 +2,8 @@ import string
 
 
 BASE_CHARACTERS = string.ascii_letters + string.digits
-SAFE_CHARACTERS = frozenset(BASE_CHARACTERS + '-')
-KEY_LENGTH = (6, 128)
+SAFE_CHARACTERS = frozenset(BASE_CHARACTERS + '-_.')
+KEY_LENGTH = (2, 128)
 NONCE_LENGTH = (6, 128)
 SECRET_LENGTH = (6, 128)
 
