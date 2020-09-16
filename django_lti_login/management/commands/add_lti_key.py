apluslms/django-lti-login

commit 39c638f1b836d3654507ab9e69d3dd31f06ff178

Add force option to add_lti_key option

django_lti_login/management/commands/add_lti_key.py
@@ -7,6 +7,8 @@ class Command(BaseCommand):
     help = 'Adds a new lti login key and generates a secret for it'
 
     def add_arguments(self, parser):
+        parser.add_argument('-f', '--force', action='store_true',
+                            help="Overwrite existing keys")
         parser.add_argument('-k', '--key',
                             help="The key. Only alphanumerals are allowed. Default: generate random")
         parser.add_argument('-s', '--secret',
@@ -23,9 +25,13 @@ class Command(BaseCommand):
         lticlient = None
         for attempt in range(1000):
             # create new
-            lticlient = LTIClient()
-            if key:
-                lticlient.key = key
+            if options['force'] and key:
+                try:
+                    lticlient = LTIClient.objects.get(key=key)
+                except LTIClient.DoesNotExist:
+                    lticlient = LTIClient(key=key)
+            else:
+                lticlient = LTIClient(key=key)
             if secret:
                 lticlient.secret = secret
             lticlient.description = desc

commit d8cbe154318c201f916504a8e51311f787147619

Fix LTI key autogeneration
Changes in commit 39c638f1b836d3654507ab9e69d3dd31f06ff178 caused a
situation where it was impossible to generate an LTI key with an
automatically generated key name.

django_lti_login/management/commands/add_lti_key.py
@@ -25,13 +25,16 @@ class Command(BaseCommand):
         lticlient = None
         for attempt in range(1000):
             # create new
-            if options['force'] and key:
-                try:
-                    lticlient = LTIClient.objects.get(key=key)
-                except LTIClient.DoesNotExist:
+            if key:
+                if options['force']:
+                    try:
+                        lticlient = LTIClient.objects.get(key=key)
+                    except LTIClient.DoesNotExist:
+                        lticlient = LTIClient(key=key)
+                else:
                     lticlient = LTIClient(key=key)
             else:
-                lticlient = LTIClient(key=key)
+                lticlient = LTIClient()
             if secret:
                 lticlient.secret = secret
             lticlient.description = desc