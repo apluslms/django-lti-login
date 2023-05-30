234b323417b82181ba2e17cc86c73993aa79fc58
Markku Riekkinen <markku.riekkinen@aalto.fi>
2023-05-30 18:52:19 +0300

    Update package classifiers

--- setup.py
+++ setup.py
@@ -47,13 +47,12 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
     ],
 
